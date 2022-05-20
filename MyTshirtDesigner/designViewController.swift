393400a41d08765341f0ccb60f9ae9df4de4abaa
stephanmarkovski <smarkovski4271@stu.d214.org>
2022-05-20 10:52:52 -0500

    added a anti crash system for the label

--- MyTshirtDesigner/designViewController.swift
+++ MyTshirtDesigner/designViewController.swift
@@ -159,13 +159,16 @@ class designViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
             if labell == labell {
                 
                 
-                    labell.font = UIFont(name: selectedFont, size: sum!)
+                labell.font = UIFont(name: selectedFont, size: sum ?? 60)
+                       
                 labell.textColor = colorValue
                 
                 labell.text = "\(addedText!)"
-                
+           
             }
-
+                
+            
+                                       
             })
         
         textAlert.view.addSubview(pickerViewFont)
@@ -302,6 +305,7 @@ class designViewController: UIViewController, UIPickerViewDataSource, UIPickerVi
         @IBAction func editbutton(_ sender: UIBarButtonItem) {
         imagedesign.image = UIImage()
             labell.text = ""
+            
     }
     // template button
     @IBAction func TemplateButton(_ sender: Any) {
